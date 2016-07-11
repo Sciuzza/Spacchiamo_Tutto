7363001734bcc06c0ca418b50a9fec772261e81d
dbga_31 <dbga_31@dbga.local>
2016-07-11 14:16:35 +0200

    Arricchimento UI con piccole funzionalità dinamiche

---
+++ Assets/Scripts/UI Scripts/UIAbilitiesScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Spacchiamo {
+	
+	internal abstract class UIAbilitiesScript : MonoBehaviour {
+
+		private UISingleTooltipScript singleTooltip;
+
+		protected internal virtual void Awake () {
+
+			this.singleTooltip = this.GetComponentInChildren <UISingleTooltipScript> (true);
+
+		}
+
+		public virtual void SetAbilityTooltipVisible () {
+
+			this.singleTooltip.gameObject.SetActive (true);
+
+		}
+
+		public virtual void SetAbilityTooltipInvisible () {
+
+			this.singleTooltip.gameObject.SetActive (false);
+
+		}
+		
+	}
+
+}
