10320947d91215be24434e9d0b33254e9e32e369
dbga_31 <dbga_31@dbga.local>
2016-07-21 11:39:58 +0200

    Ora la vita dovrebbe poter sia scendere che salire (feedback visivo, si intende; non è logica di gameplay, è HUD); eseguite anche ottimizzazioni ed adeguata spaziatura di codice (revisione dell'intero script, in sostanza); manca il link con la logica della vita.

--- Assets/Scripts/UI Scripts/UILifePanelScript.cs
+++ Assets/Scripts/UI Scripts/UILifePanelScript.cs
@@ -20,379 +20,250 @@ namespace Spacchiamo {
 				switch (life) {
 
 				case 20:
+					
 					heartScript.SetFullHeart ();
 					break;
+
 				case 19:
-					if (heartScript.name == "Heart 9") {
+					
+					if (heartScript.name == "Heart 9")
 						heartScript.SetHalfHeart ();
-					}
+
+					else 
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 18:
-					if (heartScript.name == "Heart 9") {
+					
+					if (heartScript.name == "Heart 9")
 						heartScript.SetEmptyHeart ();
-					}
+					
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 17:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetHalfHeart ();
-					}
+					
+					if ( (heartScript.name == "Heart 9") || (heartScript.name == "Heart 8") ) {
+						
+						if (heartScript.name == "Heart 8")
+							heartScript.SetHalfHeart ();
+
+						else
+							heartScript.SetEmptyHeart ();
+
+					} else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 16:
-					if (heartScript.name == "Heart 9") {
+					
+					if ( (heartScript.name == "Heart 9") || (heartScript.name == "Heart 8") )
 						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
+
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 15:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetHalfHeart ();
-					}
+					
+					if ( (heartScript.name == "Heart 9") || (heartScript.name == "Heart 8") || (heartScript.name == "Heart 7") ) {
+
+						if (heartScript.name == "Heart 7")
+							heartScript.SetHalfHeart ();
+
+						else
+							heartScript.SetEmptyHeart ();
+
+					} else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 14:
-					if (heartScript.name == "Heart 9") {
+					
+					if ( (heartScript.name == "Heart 9") || (heartScript.name == "Heart 8") || (heartScript.name == "Heart 7") )
 						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
+
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 13:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetHalfHeart ();
-					}
+					
+					if ( (heartScript.name == "Heart 9") || (heartScript.name == "Heart 8") || (heartScript.name == "Heart 7") || (heartScript.name == "Heart 6") ) {
+
+						if (heartScript.name == "Heart 6")
+							heartScript.SetHalfHeart ();
+
+						else
+							heartScript.SetEmptyHeart ();
+
+					} else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 12:
-					if (heartScript.name == "Heart 9") {
+					
+					if ( (heartScript.name == "Heart 9") || (heartScript.name == "Heart 8") || (heartScript.name == "Heart 7") || (heartScript.name == "Heart 6") )
 						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
+
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 11:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetHalfHeart ();
-					}
+					
+					if ( (heartScript.name == "Heart 9") || (heartScript.name == "Heart 8") || (heartScript.name == "Heart 7") || (heartScript.name == "Heart 6") || (heartScript.name == "Heart 5") ) {
+
+						if (heartScript.name == "Heart 5")
+							heartScript.SetHalfHeart ();
+
+						else
+							heartScript.SetEmptyHeart ();
+
+					} else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 10:
-					if (heartScript.name == "Heart 9") {
+					
+					if ( (heartScript.name == "Heart 9") || (heartScript.name == "Heart 8") || (heartScript.name == "Heart 7") || (heartScript.name == "Heart 6") || (heartScript.name == "Heart 5") )
 						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
+
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 9:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetHalfHeart ();
-					}
+					
+					if ( !(heartScript.name == "Heart 0") && !(heartScript.name == "Heart 1") && !(heartScript.name == "Heart 2") && !(heartScript.name == "Heart 3") ) {
+
+						if (heartScript.name == "Heart 4")
+							heartScript.SetHalfHeart ();
+
+						else
+							heartScript.SetEmptyHeart ();
+
+					} else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 8:
-					if (heartScript.name == "Heart 9") {
+					
+					if ( !(heartScript.name == "Heart 0") && !(heartScript.name == "Heart 1") && !(heartScript.name == "Heart 2") && !(heartScript.name == "Heart 3") )
 						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
+
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 7:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 3") {
-						heartScript.SetHalfHeart ();
-					}
+					
+					if ( !(heartScript.name == "Heart 0") && !(heartScript.name == "Heart 1") && !(heartScript.name == "Heart 2") ) {
+
+						if (heartScript.name == "Heart 3")
+							heartScript.SetHalfHeart ();
+
+						else
+							heartScript.SetEmptyHeart ();
+
+					} else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 6:
-					if (heartScript.name == "Heart 9") {
+					
+					if ( !(heartScript.name == "Heart 0") && !(heartScript.name == "Heart 1") && !(heartScript.name == "Heart 2") )
 						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 3") {
-						heartScript.SetEmptyHeart ();
-					}
+
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 5:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 3") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 2") {
-						heartScript.SetHalfHeart ();
-					}
+					
+					if ( !(heartScript.name == "Heart 0") && !(heartScript.name == "Heart 1") ) {
+
+						if (heartScript.name == "Heart 2")
+							heartScript.SetHalfHeart ();
+
+						else
+							heartScript.SetEmptyHeart ();
+
+					} else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 4:
-					if (heartScript.name == "Heart 9") {
+					
+					if ( !(heartScript.name == "Heart 0") && !(heartScript.name == "Heart 1") )
 						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 3") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 2") {
-						heartScript.SetEmptyHeart ();
-					}
+
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 3:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 3") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 2") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 1") {
-						heartScript.SetHalfHeart ();
-					}
+					
+					if ( !(heartScript.name == "Heart 0") ) {
+
+						if (heartScript.name == "Heart 1")
+							heartScript.SetHalfHeart ();
+
+						else
+							heartScript.SetEmptyHeart ();
+
+					} else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 2:
-					if (heartScript.name == "Heart 9") {
+					
+					if ( !(heartScript.name == "Heart 0") )
 						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 3") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 2") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 1") {
-						heartScript.SetEmptyHeart ();
-					}
+
+					else
+						heartScript.SetFullHeart ();
+					
 					break;
+
 				case 1:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 3") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 2") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 1") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 0") {
+					
+					if (heartScript.name == "Heart 0")
 						heartScript.SetHalfHeart ();
-					}
+
+					else
+						heartScript.SetEmptyHeart ();
+					
 					break;
+
 				case 0:
-					if (heartScript.name == "Heart 9") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 8") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 7") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 6") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 5") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 4") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 3") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 2") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 1") {
-						heartScript.SetEmptyHeart ();
-					}
-					if (heartScript.name == "Heart 0") {
-						heartScript.SetEmptyHeart ();
-					}
+					
+					heartScript.SetEmptyHeart ();
+
 					break;
+
 				default:
+					
+					Debug.LogError ("NON RIESCO A MODIFICARE IL FEEDBACK SULLA VITA!");
+
 					break;
 
 				}
