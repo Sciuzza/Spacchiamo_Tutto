0b37fa64580b7c93c19be95bfb2028bc9ec2f98d
dbga_68 <dbga_68@dbga.local>
2016-07-28 10:01:21 +0200

    menu

---
+++ Assets/Fonts/UI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    public Text text;
+    public Color mouseOverColor;
+    public Color standardColor;
+
+    public void ChangeColorRed()
+    {
+        text.color = Color.red;
+
+        
+    }
+
+    public void ChangeColorBlack()
+    {
+        text.color = Color.black;
+
+    }
+
+}
