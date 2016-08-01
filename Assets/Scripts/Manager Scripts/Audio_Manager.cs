75d299240236d971dfa4e16ad0497079866b4ed5
dbga_31 <dbga_31@dbga.local>
2016-08-01 12:23:13 +0200

    GameObject, figlio di Audio Manager, con soundtrack, pronto; array che prende Audiosources e fa partire/stoppare le varie track, pronti (deve tutto essere chiamato da Game controller).

--- Assets/Scripts/Manager Scripts/Audio_Manager.cs
+++ Assets/Scripts/Manager Scripts/Audio_Manager.cs
@@ -6,6 +6,8 @@ namespace Spacchiamo
     public class Audio_Manager : MonoBehaviour
     {
 
+		public AudioSource[] soundtrack;
+
         [HideInInspector]
         public static Audio_Manager instance = null;
 
@@ -17,6 +19,192 @@ namespace Spacchiamo
                 Destroy(gameObject);
         }
 
+
+		public void TakingSoundtrackRef () {
+
+			soundtrack = this.transform.FindChild ("Soundtrack Audio").GetComponents <AudioSource> ();
+		}
+
+
+		#region Playing_Soundtrack_Audio
+		public void PlayFirstLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play")
+					track.Play ();
+
+			}
+
+		}
+
+		public void PlaySecondLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play2")
+					track.Play ();
+
+			}
+
+		}
+
+		public void PlayThirdLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play3")
+					track.Play ();
+
+			}
+
+		}
+
+		public void PlayFourthLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play4")
+					track.Play ();
+
+			}
+
+		}
+
+		public void PlayFifthLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play5")
+					track.Play ();
+
+			}
+
+		}
+
+		public void PlayLobbyMenuSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Menu_lobby")
+					track.Play ();
+
+			}
+
+		}
+
+		public void PlayBossSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Music_Boss")
+					track.Play ();
+
+			}
+
+		}
+
+		public void PlayGameOverSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Musica_GameOver")
+					track.Play ();
+
+			}
+
+		}
+		#endregion
+
+		#region Stop_Playing_Soundtrack_Audio
+		public void StopFirstLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play")
+					track.Stop ();
+
+			}
+
+		}
+
+		public void StopSecondLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play2")
+					track.Stop ();
+
+			}
+
+		}
+
+		public void StopThirdLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play3")
+					track.Stop ();
+
+			}
+
+		}
+
+		public void StopFourthLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play4")
+					track.Stop ();
+
+			}
+
+		}
+
+		public void StopFifthLevelSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Level_play5")
+					track.Stop ();
+
+			}
+
+		}
+
+		public void StopLobbyMenuSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Menu_lobby")
+					track.Stop ();
+
+			}
+
+		}
+
+		public void StopBossSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Music_Boss")
+					track.Stop ();
+
+			}
+
+		}
+
+		public void StopGameOverSoundtrack () {
+
+			foreach (var track in soundtrack) {
+
+				if (track.clip.name == "Musica_GameOver")
+					track.Stop ();
+
+			}
+
+		}
+		#endregion
       
     }
 }
